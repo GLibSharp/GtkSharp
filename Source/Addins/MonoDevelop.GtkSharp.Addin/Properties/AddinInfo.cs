c3c4cbe5b9411b6795386fb337a24f650abac3ce
cra0zy <cra0zy@gmail.com>
2017-09-23 14:32:50 +0200

    Base Gtk# MD Addin

---
+++ Source/Addins/MonoDevelop.GtkSharp.Addin/Properties/AddinInfo.cs
@@ -0,0 +1,10 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin("MonoDevelop.GtkSharp.Addin", Version="1.0")]
+
+[assembly: AddinName("Gtk# Addin")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Provides modern Gtk# file / project templates and glade file integration.")]
+[assembly: AddinAuthor("cra0zy")]
+[assembly: AddinUrl("https://github.com/gtk-sharp/gtk-sharp")]
