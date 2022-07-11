89a5256130e37a2b89e171625ed236c51e193cc2
Andoni Morales Alastruey <amorales@fluendo.com>
2022-07-11 21:15:56 +0200

    Add first unit tests for arrays
    Split integrations tests and unit tests

---
+++ Source/tests/generator/Generator.Tests.Unit/UnitTests.cs
@@ -0,0 +1,245 @@
+﻿using Regress;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using static Regress.Global;
+
+namespace Generator.Tests {
+	public class UnitTests {
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp() {
+			var builddir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+			builddir = Path.Combine(builddir, "..", "..", "..", "..", "..", "..", "..", "builddir");
+			string prefix;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+				prefix = Path.GetFullPath(Path.Combine(builddir, "prefix", "bin"));
+			} else {
+				prefix = Path.GetFullPath(Path.Combine(builddir, "prefix", "lib"));
+
+			}
+			var regressDir = Path.GetFullPath(Path.Combine(builddir, "Source", "tests", "generator", "regress"));
+			NativeLibraryResolver.Init(new List<string>
+			{
+				prefix,
+				regressDir
+			});
+		}
+
+		[TearDown]
+		public void TearDown() {
+			try {
+			} catch { }
+		}
+
+		[Test]
+		public void Array_Callback() {
+			(int[] one, string[] two)? res = null;
+			TestCallbackArray cb = new TestCallbackArray(
+				(int[] one, ulong one_length, string[] two, int tow_length) => {
+					res = (one, two);
+					return 1;
+				}
+			);
+			var ret = TestArrayCallback(cb);
+
+			Assert.IsNotNull(res);
+			var notNullRes = res!.Value;
+			Assert.That(ret, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Array_Callback_InOut() {
+			var rets = new int[2];
+			var callbackCount = 0;
+
+			TestCallbackArrayInOut cb = new TestCallbackArrayInOut(
+				(ref int[] ints, ref int length) => {
+					rets[callbackCount] = length;
+					length--;
+					callbackCount++;
+					ints = new int[length];
+					for (int i = 0; i < length; i++) {
+						ints[i] = i - (2 - callbackCount);
+					}
+				}
+			);
+
+			var ret = TestArrayInoutCallback(cb);
+
+			Assert.That(ret, Is.EqualTo(3));
+			Assert.That(callbackCount, Is.EqualTo(2));
+			Assert.That(rets[0], Is.EqualTo(5));
+			Assert.That(rets[1], Is.EqualTo(4));
+		}
+
+		[Ignore("FIXME: Regress types not registered")]
+		[Test]
+		public void Array_FixedOut_Objects() {
+			var res = TestArrayFixedOutObjects();
+			Assert.That(res.Length, Is.EqualTo(2));
+			Assert.IsTrue(res[0] is TestObj);
+			Assert.IsTrue(res[1] is TestObj);
+		}
+
+		[Test]
+		public void Array_FixedSize_Int_In() {
+			var ints = new int[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayFixedSizeIntIn(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_FixedSize_Int_Out() {
+			var res = TestArrayFixedSizeIntOut();
+
+			Assert.That(res.Length, Is.EqualTo(5));
+			Assert.That(res[0], Is.EqualTo(0));
+			Assert.That(res[1], Is.EqualTo(1));
+			Assert.That(res[2], Is.EqualTo(2));
+			Assert.That(res[3], Is.EqualTo(3));
+			Assert.That(res[4], Is.EqualTo(4));
+		}
+
+		[Test]
+		public void Array_gint8_In() {
+			var ints = new sbyte[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayGint8In(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_gint16_In() {
+			var ints = new short[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayGint16In(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_gint32_In() {
+			var ints = new int[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayGint32In(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_gint64_In() {
+			var ints = new long[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayGint64In(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_GType_In() {
+			var gTypes = new GLib.GType[3] { FooBoxed.GType, FooBuffer.GType, FooObject.GType };
+			var res = TestArrayGtypeIn(gTypes);
+			Assert.That(res, Is.EqualTo("[RegressFooBoxed,RegressFooBuffer,RegressFooObject,]"));
+		}
+
+		[Test]
+		public void Array_int_In() {
+			var ints = new int[5] { 0, 1, 2, 3, 4 };
+			var res = TestArrayIntIn(ints);
+			Assert.That(res, Is.EqualTo(1 + 2 + 3 + 4));
+		}
+
+		[Test]
+		public void Array_int_Out() {
+			var ints = TestArrayIntOut();
+			Assert.That(ints, Is.EqualTo(new int[5] { 0, 1, 2, 3, 4 }));
+		}
+
+		[Test]
+		public void Array_int_InOut() {
+			var ints = new int[5] { 0, 1, 2, 3, 4 };
+			TestArrayIntInout(ref ints);
+			Assert.That(ints, Is.EqualTo(new int[4] { 2, 3, 4, 5 }));
+		}
+
+		[Test]
+		public void Array_int_NullOut() {
+			TestArrayIntNullOut(out int[] arr, out int len);
+			Assert.IsNull(arr);
+		}
+
+		[Test]
+		public void Array_Static_int_In() {
+			var ints = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			TestArrayStaticInInt(ints);
+		}
+
+		[Ignore("FIXME: Transfer ful crash, copy needed")]
+		[Test]
+		public void Array_Struct_In_Full() {
+			var structs = new TestStructA[2] {
+				new TestStructA {SomeInt = 201},
+				new TestStructA {SomeInt = 202}
+			};
+			TestArrayStructInFull(structs, (ulong)structs.Length);
+		}
+
+		[Test]
+		public void Array_Struct_In_None() {
+			var structs = new TestStructA[3] {
+				new TestStructA {SomeInt = 301},
+				new TestStructA {SomeInt = 302},
+				new TestStructA {SomeInt = 303},
+			};
+			TestArrayStructInNone(structs, (ulong)structs.Length);
+		}
+
+		[Ignore("FIXME: array lenght parameter not supported")]
+		[Test]
+		public void Array_Struct_Out() {
+			TestArrayStructOut(out TestStructA[] structs, out int len);
+			Assert.That(structs.Length, Is.EqualTo(3));
+			Assert.That(structs[0].SomeInt, Is.EqualTo(22));
+			Assert.That(structs[1].SomeInt, Is.EqualTo(33));
+			Assert.That(structs[2].SomeInt, Is.EqualTo(44));
+		}
+
+		[Ignore("FIXME: caller-allocates not implemented")]
+		[Test]
+		public void Array_Struct_Out_CallerAlloc() {
+			TestArrayStructOutCallerAlloc(out TestStructA[] structs, out ulong len);
+			Assert.That(structs.Length, Is.EqualTo(3));
+			Assert.That(structs[0].SomeInt, Is.EqualTo(22));
+			Assert.That(structs[1].SomeInt, Is.EqualTo(33));
+			Assert.That(structs[2].SomeInt, Is.EqualTo(44));
+		}
+
+
+		[Ignore("FIXME: array lenght parameter not supported")]
+		[Test]
+		public void Array_Struct_Out_Container() {
+			TestArrayStructOutContainer(out TestStructA[] structs, out ulong len);
+			Assert.That(structs.Length, Is.EqualTo(5));
+			Assert.That(structs[0].SomeInt, Is.EqualTo(11));
+			Assert.That(structs[1].SomeInt, Is.EqualTo(13));
+			Assert.That(structs[2].SomeInt, Is.EqualTo(17));
+			Assert.That(structs[3].SomeInt, Is.EqualTo(19));
+			Assert.That(structs[4].SomeInt, Is.EqualTo(23));
+		}
+
+
+		[Test]
+		public void Array_Struct_Out_Full_Fixed() {
+			var res = TestArrayStructOutFullFixed();
+
+			Assert.That(res.Length, Is.EqualTo(4));
+			Assert.That(res[0].SomeInt, Is.EqualTo(2));
+			Assert.That(res[1].SomeInt, Is.EqualTo(3));
+			Assert.That(res[2].SomeInt, Is.EqualTo(5));
+			Assert.That(res[3].SomeInt, Is.EqualTo(7));
+		}
+
+		[Ignore("FIXME: segfault, copy is needed with transfer none")]
+		[Test]
+		public void Array_Struct_Out_None() {
+			TestArrayStructOutNone(out TestStructA[] structs, out ulong len);
+			Assert.That(structs.Length, Is.EqualTo(3));
+			Assert.That(structs[0].SomeInt, Is.EqualTo(111));
+			Assert.That(structs[1].SomeInt, Is.EqualTo(222));
+			Assert.That(structs[2].SomeInt, Is.EqualTo(333));
+		}
+	}
+}
---
+++ Source/tests/generator/NativeLibraryResolver.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Generator.Tests {
+	public static class NativeLibraryResolver {
+		static readonly Dictionary<string, Dictionary<OSPlatform, string>> LIBS_MAPPING = new()
+		{
+
+			{
+				"regress-1.0",
+				new Dictionary<OSPlatform, string> {
+					{OSPlatform.Linux, "libregress-1.0.so" },
+					{OSPlatform.Windows, "libregress-1.0.dll" },
+					{OSPlatform.OSX, "libregress-1.0.dylib" },
+				}
+			},
+			{
+				"glib-2.0-0.dll",
+				new Dictionary<OSPlatform, string> {
+					{OSPlatform.Linux, "libglib-2.0.so.0" },
+					{OSPlatform.Windows, "glib-2.0-0.dll" },
+					{OSPlatform.OSX, "libglib-2.0.0.dylib" },
+				}
+			},
+			{
+				"gobject-2.0-0.dll",
+				new Dictionary<OSPlatform, string> {
+					{OSPlatform.Linux, "libgobject-2.0.so.0" },
+					{OSPlatform.Windows, "gobject-2.0-0.dll" },
+					{OSPlatform.OSX, "libgobject-2.0.0.dylib" },
+				}
+			},
+			{
+				"gio-2.0-0.dll",
+				new Dictionary<OSPlatform, string> {
+					{OSPlatform.Linux, "libgio-2.0.so.0" },
+					{OSPlatform.Windows, "gio-2.0-0.dll" },
+					{OSPlatform.OSX, "libgio-2.0.0.dylib" },
+				}
+			},
+
+		};
+
+		static List<string> searchDirs = new List<string>();
+
+		public static void Init(List<string> searchDirs) {
+			NativeLibraryResolver.searchDirs = searchDirs;
+			// glib-sharp
+			NativeLibrary.SetDllImportResolver(typeof(GLib.AbiField).Assembly, ImportResolver);
+			// gio-sharp
+			NativeLibrary.SetDllImportResolver(typeof(GLib.Application).Assembly, ImportResolver);
+			// regress
+			NativeLibrary.SetDllImportResolver(typeof(Regress.AnAnonymousUnion).Assembly, ImportResolver);
+		}
+
+		static IntPtr ImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath) {
+			IntPtr libPtr = IntPtr.Zero;
+			foreach (var dir in searchDirs) {
+				libPtr = TryLoad(libraryName, assembly, searchPath, dir);
+				if (libPtr != IntPtr.Zero) {
+					break;
+				}
+			}
+			return libPtr;
+		}
+
+		private static IntPtr TryLoad(string libName, Assembly assembly, DllImportSearchPath? searchPath, string searchDir) {
+			IntPtr libHandle;
+			string libraryName = libName;
+
+			if (LIBS_MAPPING.TryGetValue(libName, out var osToNameDict)) {
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+					libraryName = osToNameDict[OSPlatform.OSX];
+				} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+					libraryName = osToNameDict[OSPlatform.Windows];
+				} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+					libraryName = osToNameDict[OSPlatform.Windows];
+				} else {
+					throw new NotSupportedException("Operating system not supported");
+				}
+				libraryName = Path.Join(searchDir, libraryName);
+			}
+
+			if (searchPath != null) {
+				NativeLibrary.TryLoad(libraryName, assembly, searchPath, out libHandle);
+			} else {
+				NativeLibrary.TryLoad(libraryName, assembly, DllImportSearchPath.ApplicationDirectory, out libHandle);
+			}
+
+			return libHandle;
+		}
+	}
+}
