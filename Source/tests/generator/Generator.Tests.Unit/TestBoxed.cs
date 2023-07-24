411a0caf57f688a85917c5325d495c902c58aaa5
Andoni Morales Alastruey <ylatuya@gmail.com>
2023-07-24 15:50:13 +0200

    Add the missing regress unit tests

---
+++ Source/tests/generator/Generator.Tests.Unit/TestBoxed.cs
@@ -0,0 +1,20 @@
+﻿using static Regress.Global;
+
+namespace Generator.Tests
+{
+    [TestFixture]
+    public class TestBoxed
+    {
+        [Test]
+        public void Boxed_NotAStatic()
+        {
+            TestBoxedsNotAStatic();
+        }
+
+        [Test]
+        public void Boxed_NotAMethod()
+        {
+            TestBoxedsNotAMethod(new Regress.TestBoxed());
+        }
+    }
+}
